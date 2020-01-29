10e811e7db8b355f3060d34a505ebc823a4a87ff
Sean <seanmgray77@gmail.com>
2020-01-28 19:22:48 -0800

    Adding New Job Changes

--- force-app/main/default/lwc/newJobLWC/newJobLWC.js
+++ force-app/main/default/lwc/newJobLWC/newJobLWC.js
@@ -10,17 +10,26 @@
  * 1.0    1/23/2020   Sean Gray     Initial Version
 **/
 import { LightningElement, track } from 'lwc';
-import SearchAccountRoles from '@salesforce/apex/NewJobController.GetAccountRoles';
+//import SearchAccountRoles from '@salesforce/apex/NewJobController.GetAccountRoles';
+import SearchProperties from '@salesforce/apex/NewJobController.GetProperties';
+import checkId from '@salesforce/apex/NewJobController.CheckId';
 
+const DELAY = 900;
 export default class NewJobLWC extends LightningElement {
-
+@track testingProperty;
+    @track Properties; 
+@track searchKey;
 @track NewCaller = false;
 @track NewAccount = false;
 @track NewProperty = false;
 @track PropertyID;
 @track PropertyPicked = false;
-@track AccountRoles = [{}];
+@track AccountRoles;
+@track AccountRolesSelected = false;
 @track AccountRolesRecieved ='';
+@track inputDisabled = true;
+@track PropertySelected = false;
+@track PropertySelectedField;
 
 ToggleNewCaller(){
     this.NewAccount = false;
@@ -44,32 +53,74 @@ ToggleNewProperty(){
         this.NewProperty = false;
     }
 }
-PropertyChange(event){
-    this.PropertyID = event.detail.value;
-    console.log(this.PropertyID.length);
-    if(this.PropertyID.length > 17){
-    this.PropertyPicked = true;
+AddNewRow(){
+    this.AccountRoleNew(Name ='test');
+    this.AccountRoles.push(this.AccountRoleNew);
+}
+// PropertyChange(event){
+//     this.PropertyID = event.detail.value;
+//     console.log(this.PropertyID.length);
+//     if(this.PropertyID.length > 17){
+//     this.PropertyPicked = true;
     
-    var PropertyJSON = {'PropertyId': this.PropertyID};
-    var PackagedJSON = JSON.stringify(PropertyJSON);
+//     var PropertyJSON = {'PropertyId': this.PropertyID};
+//     var PackagedJSON = JSON.stringify(PropertyJSON);
     
-    SearchAccountRoles({PropertyId:PackagedJSON})
-    .then(result => {
-        this.AccountRolesRecieved = result;
-        for (var i = 0; i < this.AccountRolesRecieved.length; i++){
-                this.AccountRoles.push({name: this.AccountRolesRecieved[i],}) 
-            }
-        console.log('records' + this.records);
-        this.error = undefined;
-        //console.log(' records ', this.records);
+//     SearchAccountRoles({PropertyId:PackagedJSON})
+//     .then(result => {
+//         this.AccountRolesRecieved = result;
+//         for (var i = 0; i < this.AccountRolesRecieved.length; i++){
+//                 this.AccountRoles.push({name: this.AccountRolesRecieved[i],}) 
+//             }
+//         console.log('records' + this.records);
+//         this.error = undefined;
+//         //console.log(' records ', this.records);
+//     })
+//     .catch(error => {
+//         this.error = error;
+//         this.records = undefined;
+//     });
+
+// }
+// }
+populatePropertyField(event){
+    
+   // console.log('Property Id first is + ' + this.Property.Id);
+    //console.log('Property Id first is + ' + this.testingProperty);
+    //this.PropertyID = event.detail.value;
+    this.Properties = '';
+    this.PropertySelected = true;
+    this.testingProperty = event.target.value;
+    this.PropertySelectedField = event.target.value;
+    console.log('Testing name alert ' + this.PropertySelectedField.name);
+    console.log('PropertyID ' + this.testingProperty);
+
+    checkId({propId:this.testingProperty.Id}).then(result => {
+        this.AccountRoles = result;
+        this.AccountRolesSelected = true;
     })
-    .catch(error => {
-        this.error = error;
-        this.records = undefined;
-    });
+}
+PropertyChanged(event){
+    
+    // window.clearTimeout(this.delayTimeout);
+    console.log('testing ');
+    window.clearTimeout(this.delayTimeout);
+       this.searchKey = event.target.value;
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        //this.delayTimeout = setTimeout(() => {
+            // eslint-disable-next-line @lwc/lwc/no-async-operation
+            this.delayTimeout = setTimeout(() => {
+                SearchProperties({searchKey : this.searchKey})
+                .then(result => {
+                    this.Properties = result;
+                    console.log('Properties ' + this.Properties);
+                })
+                
+                .catch(error => {
+                    this.error = error;
+                });
+            }, DELAY);
+        // });
 
 }
-}
-
-
 }
