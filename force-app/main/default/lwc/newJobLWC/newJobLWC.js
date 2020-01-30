119d8fbfddb05843fce8a1f446d66a0158bda7e5
Sean <seanmgray77@gmail.com>
2020-01-30 09:12:02 -0800

    First commit to new branch

--- force-app/main/default/lwc/newJobLWC/newJobLWC.js
+++ force-app/main/default/lwc/newJobLWC/newJobLWC.js
@@ -4,7 +4,7 @@
  * @Author             : Sean Gray
  * @Group              : 
  * @Last Modified By   : Sean Gray
- * @Last Modified On   : 1/23/2020, 3:08:44 PM
+ * @Last Modified On   : 1/30/2020, 9:00:20 AM
  * @Modification Log   : 
  * Ver       Date            Author      		    Modification
  * 1.0    1/23/2020   Sean Gray     Initial Version
@@ -24,13 +24,17 @@ export default class NewJobLWC extends LightningElement {
 @track NewProperty = false;
 @track PropertyID;
 @track PropertyPicked = false;
-@track AccountRoles;
+@track AccountRoles = [{}];
 @track AccountRolesSelected = false;
 @track AccountRolesRecieved ='';
 @track inputDisabled = true;
 @track PropertySelected = false;
 @track PropertySelectedField;
+@track PropertyValue;
 
+// connectedCallback(){
+//     this.AccountRoles.push({Name : '',})
+// }
 ToggleNewCaller(){
     this.NewAccount = false;
     if(!this.NewCaller){
@@ -54,8 +58,8 @@ ToggleNewProperty(){
     }
 }
 AddNewRow(){
-    this.AccountRoleNew(Name ='test');
-    this.AccountRoles.push(this.AccountRoleNew);
+    //this.AccountRoleNew(Name ='test');
+    this.AccountRoles.push({Name : '', Type : '', Contact : ''});
 }
 // PropertyChange(event){
 //     this.PropertyID = event.detail.value;
@@ -92,12 +96,16 @@ populatePropertyField(event){
     this.PropertySelected = true;
     this.testingProperty = event.target.value;
     this.PropertySelectedField = event.target.value;
+    this.PropertyValue = this.PropertySelectedField.Name;
     console.log('Testing name alert ' + this.PropertySelectedField.name);
     console.log('PropertyID ' + this.testingProperty);
 
     checkId({propId:this.testingProperty.Id}).then(result => {
         this.AccountRoles = result;
         this.AccountRolesSelected = true;
+        console.log('Account Roles is ' + this.AccountRoles);
+        console.log('Account Roles Name ' + this.AccountRoles.Name);
+        console.log('Account Roles Contact ' + this.AccountRoles.Contact_ID__c);
     })
 }
 PropertyChanged(event){
@@ -123,4 +131,51 @@ PropertyChanged(event){
         // });
 
 }
+
+getAllAccountRoleObjects() {
+    var AccountRoless = [];
+
+    //Get Value from HTML 
+    let TblRow =  Array.from(this.template.querySelectorAll('table.PMTbl tbody tr'));
+    let RowCount = TblRow.length;
+    for(let k=0; k<RowCount; k++){
+        let Name = TblRow[k].querySelector('.PMName').value;
+        let TblReg = TblRow[k].querySelectorAll('.PM_Reg');
+        let TblOt = TblRow[k].querySelectorAll('.PM_OT');
+        let TblDbl = TblRow[k].querySelectorAll('.PM_DBL');
+        
+        
+
+        let LaborCode = TblRow[k].querySelector('.PMLaborCode').value;
+        //let CWPw = TblRow[k].querySelector('.CWPw').checked;
+        
+        
+        AccountRoless.push({
+            Name: Name,PW: false,LaborCode: LaborCode,MondayDBLHours: MondayDBLHours, MondayOTHours: MondayOTHours, MondayRegHours: MondayRegHours,
+            TuesdayDBLHours: TuesdayDBLHours,TuesdayOTHours: TuesdayOTHours, TuesdayRegHours: TuesdayRegHours,
+            WednesdayDBLHours: WednesdayDBLHours,WednesdayOTHours: WednesdayOTHours, WednesdayRegHours: WednesdayRegHours,
+            ThursdayDBLHours:ThursdayDBLHours,ThursdayOTHours:ThursdayOTHours , ThursdayRegHours:ThursdayRegHours,
+            FridayDBLHours:FridayDBLHours ,FridayOTHours:FridayOTHours , FridayRegHours:FridayRegHours,
+            SaturdayDBLHours:SaturdayDBLHours ,SaturdayOTHours: SaturdayOTHours , SaturdayRegHours:SaturdayRegHours ,
+            SundayDBLHours:SundayDBLHours ,SundayOTHours:SundayOTHours , SundayRegHours:SundayRegHours
+        });
+
+    }
+    return AccountRoless;
+}
+
+
+DeletePDRow(e){
+    this.AccountRolesToDelete = this.getAllAccountRoleObjects();
+    let DeleteRowIndex = e.target.parentNode.parentNode.rowIndex;
+    this.AccountRolesToDelete.splice(DeleteRowIndex-1,1);
+
+    // this.Supervisors = this.getAllSupervisorObjects();
+    // this.CrewMembers = this.getAllCrewMembersObjects();
+}
+
+
+
+
+
 }
