25af56109b7cd2efbe8b20631b40fadd95a0b749
Sean <seanmgray77@gmail.com>
2020-02-11 07:33:45 -0800

    Finishing Up CloneJob

--- force-app/main/default/lwc/cloneJobLWC/cloneJobLWC.js
+++ force-app/main/default/lwc/cloneJobLWC/cloneJobLWC.js
@@ -18,36 +18,41 @@ import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
 const fields = [
     'ATI_Job__c.Name',  'ATI_Job__c.Job_Name__c', 
-    // 'ATI_Job__c.Job_Number__c', 'ATI_Job__c.Account__c', 'ATI_Job__c.Contact__c', 'ATI_Job__c.Taken_By__c', 'ATI_Job__c.Customer__c',
-    // 'ATI_Job__c.Lead_Source__c', 'ATI_Job__c.Year_Structure_Built__c', 'ATI_Job__c.Date_of_Loss__c', 'ATI_Job__c.Description__c', 'ATI_Job__c.Job_Class__c', 'ATI_Job__c.Probability__c',
-    // 'ATI_Job__c.Project_Site_Contact_Name__c', 'ATI_Job__c.Project_Site_Contact_Account__c', 'ATI_Job__c.Office2__c',  'ATI_Job__c.Stage__c', 
-    // 'ATI_Job__c.Project_type__c', 
-    // 'ATI_Job__c.Referred_by__c', 
-    // 'ATI_Job__c.CloseDate__c', 
-    // 'ATI_Job__c.Contact_Information__c', 
-    // 'ATI_Job__c.City_of_LA__c', 
-    // 'ATI_Job__c.Project_Site_Address__c', 
-    // 'ATI_Job__c.Project_Site_Address_2__c', 
-    // 'ATI_Job__c.Project_Site_City__c', 
-    // 'ATI_Job__c.Project_Site_State__c', 
-    // 'ATI_Job__c.Project_Site_Zipcode__c', 
-    // 'ATI_Job__c.Project_Site_Contact_Email__c', 
-    // 'ATI_Job__c.no_Email_Available__c', 
-    // 'ATI_Job__c.Policy__c', 
-    // 'ATI_Job__c.Deductible__c', 
-    // 'ATI_Job__c.Claim__c', 
-    // 'ATI_Job__c.Cont_P_O_Client_Job__c', 
-    // 'ATI_Job__c.PO__c', 
-    // 'ATI_Job__c.Send_Prelim__c'
+    'ATI_Job__c.Job_Number__c', 'ATI_Job__c.Account__c', 'ATI_Job__c.Contact__c', 'ATI_Job__c.Taken_By__c', 
+    'ATI_Job__c.Lead_Source__c', 'ATI_Job__c.Year_Structure_Built__c', 
+    'ATI_Job__c.Date_of_Loss__c', 'ATI_Job__c.Description__c', 'ATI_Job__c.Job_Class__c', 
+    'ATI_Job__c.Project_Site_Contact_Name__c', 'ATI_Job__c.Project_Site_Contact_Account__c', 'ATI_Job__c.Office2__c',  'ATI_Job__c.Stage__c', 
+    'ATI_Job__c.Project_Type__c', 
+    'ATI_Job__c.Referred_by__c', 
+    'ATI_Job__c.Project_Manager__c',
+    'ATI_Job__c.CloseDate__c',
+    'ATI_Job__c.City_of_LA__c', 
+    'ATI_Job__c.Project_Site_Address__c', 
+    'ATI_Job__c.Project_Site_Address_2__c', 
+    'ATI_Job__c.Project_Site_City__c', 
+    'ATI_Job__c.Project_Site_State__c', 
+    'ATI_Job__c.Project_Site_Zipcode__c', 
+    'ATI_Job__c.County__c',
+    'ATI_Job__c.No_Email_Available__c', 
+    'ATI_Job__c.Policy__c', 
+    'ATI_Job__c.Deductible__c', 
+    'ATI_Job__c.Claim__c', 
+    'ATI_Job__c.Cont_P_O_Client_Job__c', 
+    'ATI_Job__c.PO__c', 
+    'ATI_Job__c.Send_Prelim__c',
+    
 ];
 export default class CloneJobLWC extends LightningElement {
     connectedCallback(){
         console.log('Record id is ' + this.recordId);
     }
+@api objectApiName;
+@track objectInfo;
 @track ContPOClientJob;
 @track PO;
 @track SendPrelim;
 @track CityOfLA;
+@track County;
 @track ProjectSiteCity;
 @track ProjectSiteState;
 @track ProjectSiteZipcode;
@@ -78,12 +83,22 @@ export default class CloneJobLWC extends LightningElement {
 @track Contact;
 @track TakenBy;
 @track Customer;
+@track ProjectDirector;
 @track No = false;
+@track loading = false;
 
 @api recordId;    
 @track Yes = false;
 @track Start = true;
-    
+
+@wire(getObjectInfo, { objectApiName: JOB_OBJECT })
+    objectInfo;   
+get recordTypeId() {
+        // Returns a map of record type Ids 
+        const rtis = this.objectInfo.data.recordTypeInfos;
+        console.log('works up to here');
+        return Object.keys(rtis).find(rti => rtis[rti].name === 'New Job (Not Approved)');
+}
 @wire(CurrentPageReference) pageRef;
 
 @wire(getObjectInfo, { objectApiName: JOB_OBJECT })
@@ -99,40 +114,42 @@ export default class CloneJobLWC extends LightningElement {
            console.log('Testing name ' + data.fields.Name.value);
             this.Name = data.fields.Name.value;
             this.JobName = data.fields.Job_Name__c.value;
-            // this.JobNumber = data.fields.Job_Number__c.value;
-            // this.Account = data.fields.Account__c.value;
-            // this.Contact = data.fields.Contact__c.value;
-            // this.TakenBy = data.fields.Taken_By__c.value;
-            // this.Customer = data.fields.Customer__c.value;
-            // this.LeadSource = data.fields.Lead_Source__c.value;
-            // this.YearStructureBuilt = data.fields.Year_Structure_Built__c.value;
-            // this.DateOfLoss = data.fields.Date_of_Loss__c.value;
-            // this.Description = data.fields.Description__c.value;
-            // this.JobClass = data.fields.Job_Class__c.value;
-            // this.Probability = data.fields.Probability__c.value;
-            // this.ProjectSiteContactName = data.fields.Project_Site_Contact_Name__c.value;
-            // this.ProjectSiteContactAccount = data.fields.Project_Site_Contact_Account__c.value;
-            // this.Office = data.fields.Office2__c.value;
-            // this.Stage = data.fields.Stage__c.value;
-            // this.ProjectType = data.fields.Project_type__c.value;
-            // this.ReferredBy = data.fields.Referred_by__c.value;
-            // this.County = data.fields.County__c.value;
-            // this.CloseDate = data.fields.CloseDate__c.value;
-            // this.ContactInformation = data.fields.Contact_Information__c.value;
-            // this.CityOfLA = data.fields.City_of_LA__c.value;
-            // this.ProjectSiteAddress = data.fields.Project_Site_Address__c.value;
-            // this.ProjectSiteAddress2 = data.fields.Project_Site_Address_2__c.value;
-            // this.ProjectSiteCity = data.fields.Project_Site_City__c.value;
-            // this.ProjectSiteState = data.fields.Project_Site_State__c.value;
-            // this.ProjectSiteZipcode = data.fields.Project_Site_Zipcode__c.value;
-            // this.ProjectSiteContactEmail = data.fields.Project_Site_Contact_Email__c.value;
-            // this.NoEmailAvailable = data.fields.no_Email_Available__c.value;
-            // this.Policy = data.fields.Policy__c.value;
-            // this.Deductible = data.fields.Deductible__c.value;
-            // this.Claim = data.fields.Claim__c.value;
-            // this.ContPOClientJob = data.fields.Cont_P_O_Client_Job__c.value;
-            // this.PO = data.fields.PO__c.value;
-            // this.SendPrelim = data.fields.Send_Prelim__c.value;
+            this.JobNumber = data.fields.Job_Number__c.value;
+            this.Account = data.fields.Account__c.value;
+            this.Contact = data.fields.Contact__c.value;
+            this.TakenBy = data.fields.Taken_By__c.value;
+         
+            this.LeadSource = data.fields.Lead_Source__c.value;
+            this.YearStructureBuilt = data.fields.Year_Structure_Built__c.value;
+            this.DateOfLoss = data.fields.Date_of_Loss__c.value;
+            this.Description = data.fields.Description__c.value;
+            this.JobClass = data.fields.Job_Class__c.value;
+           
+            this.ProjectSiteContactName = data.fields.Project_Site_Contact_Name__c.value;
+            this.ProjectSiteContactAccount = data.fields.Project_Site_Contact_Account__c.value;
+            this.Office = data.fields.Office2__c.value;
+            this.Stage = 'Qualification';
+            this.ProjectType = data.fields.Project_Type__c.value;
+            this.ReferredBy = data.fields.Referred_by__c.value;
+            this.County = data.fields.County__c.value;
+            this.CloseDate = data.fields.CloseDate__c.value;
+            this.ProjectDirector = data.fields.Project_Manager__c.value;
+
+           
+            this.CityOfLA = data.fields.City_of_LA__c.value;
+            this.ProjectSiteAddress = data.fields.Project_Site_Address__c.value;
+            this.ProjectSiteAddress2 = data.fields.Project_Site_Address_2__c.value;
+            this.ProjectSiteCity = data.fields.Project_Site_City__c.value;
+            this.ProjectSiteState = data.fields.Project_Site_State__c.value;
+            this.ProjectSiteZipcode = data.fields.Project_Site_Zipcode__c.value;
+          
+            this.NoEmailAvailable = data.fields.No_Email_Available__c.value;
+            this.Policy = data.fields.Policy__c.value;
+            this.Deductible = data.fields.Deductible__c.value;
+            this.Claim = data.fields.Claim__c.value;
+            this.ContPOClientJob = data.fields.Cont_P_O_Client_Job__c.value;
+            this.PO = data.fields.PO__c.value;
+            this.SendPrelim = data.fields.Send_Prelim__c.value;
             
 
 		}
@@ -145,16 +162,26 @@ export default class CloneJobLWC extends LightningElement {
         this.Start = false;
         this.No = true;
     }
+    handleSuccess(event){
+        console.log('before payload');
+        var payload = event.detail.value;
+        console.log(payload.id);
+        this[NavigationMixin.Navigate]({
+            type: 'standard__recordPage',
+            attributes: {
+                recordId: payload.id,
+                objectApiName: 'ATI_Job__c',
+                actionName: 'view',
+            },
+        });
+    }
+    Loading(){
+        console.log('Starting Load');
+        // this.loading = true;
+    }
     Cancel(){
         this.loading = true;
         this.dispatchEvent(new CustomEvent('closeform'));
-        // this[NavigationMixin.Navigate]({
-        //     type: 'standard__recordPage',
-        //     attributes: {
-        //         recordId: this.recordId,
-        //         objectApiName: 'ATI_Job__c',
-        //         actionName: 'view',
-        //     },
-        // });
+        
     }
 }
